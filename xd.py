7e42294d61c98cbe8d65a19160b4283832ff013a
sunsuzy <139964679+sunsuzy@users.noreply.github.com>
2023-07-31 12:13:30 +0200

    Update xd.py

--- xd.py
+++ xd.py
@@ -49,8 +49,6 @@ def main():
     product_price_feed_df = pd.read_csv("https://github.com/sunsuzy/xd/blob/799d824760c334a945e5e12b7ae8e9eef28ad674/Xindao.V2.ProductPrices-nl-nl-C26907%20(1).txt", delimiter='\t')
     print_price_feed_df = pd.read_csv("https://github.com/sunsuzy/xd/blob/799d824760c334a945e5e12b7ae8e9eef28ad674/Xindao.V2.PrintPrices-nl-nl-C26907%20(1).txt", delimiter='\t')
 
-    print(product_price_feed_df.head())
-
     descriptions = product_price_feed_df['ItemName'].unique()
     query = st.text_input('Search for a product or enter an item code')
     if query:
